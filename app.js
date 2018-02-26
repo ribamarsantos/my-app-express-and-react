efb6ff3b5cef5168631c403766ade1beccda12e4
Ribamar Santos <ribamaar@gmail.com>
2018-02-26 18:10:48 -0300

    Fixing order of routes

--- app.js
+++ app.js
@@ -6,14 +6,12 @@ const port = process.env.PORT || 5000;
 
 app.use('/', express.static(`${__dirname}/front/build`));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/front/build/index.html'));
-});
-
 app.get('/api/yo', (req, res) => {
   res.send({ payload: 'Yo!!!' });
 });
 
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/front/build/index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}, on ${app.get('env')}`));
